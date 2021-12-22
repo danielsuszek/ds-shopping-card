27eb057859d0de9d639b3177dedb683f1a6a37e2
danielsuszek <danielsuszek1@gmail.com>
2021-12-22 20:18:15 +0100

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 require("dotenv").config();
 const express = require("express");
 const productRoutes = require("./routes/productRoutes");
@@ -9,11 +11,23 @@ const app = express();
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.json({message: 'API running...'})
-})
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'))
+  })
+}
+ else {
+  app.get('/', (req, res) => {
+    res.json({message: 'API running...'})
+  })
+  
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
